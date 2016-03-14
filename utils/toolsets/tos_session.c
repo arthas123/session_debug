10bbc0be42581ee4fe5b0c50e682144bcb6b684b
n <shij@sita1.(none)>
2016-03-14 19:56:55 +0800

    314 update

--- utils/toolsets/tos_session.c
+++ utils/toolsets/tos_session.c
@@ -2,6 +2,7 @@
 #include "tos_session.h"
 #include "tos_notify.h"
 #include "session.h"
+#include "fpi_base_type.h"
 #include "fpi_debug_helper.h"
 
 static uint32_t s_sessionpool = 1;
@@ -12,8 +13,10 @@ fpi_error tos_session_create(EM_SESSION_TYPE source_type, uint32_t* session_id)
 {
 	int index = 0;
 	session_info *session_pool_addr;
-	
-	if(get_session_pool_addr(&session_pool_addr)){
+	printf("#################enter tos_session_create##################\n");
+	fpi_error result = get_session_pool_addr(&session_pool_addr);
+	printf("#################result = %d##################\n",result);
+	if(FPI_ERROR_SUCCESS == result){
 		
 		while(session_pool_addr[index].session_status != SESSION_DESTROYED)
 		{
@@ -25,7 +28,8 @@ fpi_error tos_session_create(EM_SESSION_TYPE source_type, uint32_t* session_id)
 		session_pool_addr[index].session_id = s_sessionpool++;
 		session_pool_addr[index].session_status = SESSION_CREATED;
 		session_pool_addr[index].source_type = source_type;
-	
+
+		printf("####################exit tos_session_create, session_id=%d############################\n",*session_id);
 		return FPI_ERROR_SUCCESS;
 	}
 	else{
